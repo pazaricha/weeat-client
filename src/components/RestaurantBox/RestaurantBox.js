a4644850248a48990037c1b116dcfe0d5a611a07
Paz Aricha <paz@pazaricha.com>
2018-05-15 17:07:24 +0300

    scss variables and tenbis icon

--- src/components/RestaurantBox/RestaurantBox.js
+++ src/components/RestaurantBox/RestaurantBox.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './RestaurantBox.css';
 import RestaurantIcon from '../RestaurantIcon/RestaurantIcon';
+import tenbis_image from '../../images/tenbis.jpg'
 
 const RestaurantBox = (props) => {
   const restaurant = props.restaurant;
@@ -10,7 +11,10 @@ const RestaurantBox = (props) => {
     <div className="restaurant-box">
       <RestaurantIcon icon={restaurant.cuisine.name}/>
       <div className="details">
-        <h4 title={restaurant.name}>{restaurant.name} <span>{restaurant.tenbis ? 'TENBIS' : ''}</span></h4>
+        <div className="restaurant-title">
+          <h4 title={restaurant.name}>{restaurant.name}</h4>
+          <span>{restaurant.tenbis ? <img className="tenbis" src={tenbis_image} title="Tenbis" /> : ''}</span>
+        </div>
         <p className="rating">Rating: * * * *</p>
       </div>
     </div>
